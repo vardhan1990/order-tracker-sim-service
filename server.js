26f46da74a3024cab7ad6fa77790ba1a0f55014b
Vardhan Dugar <vardhan1990@gmail.com>
2019-08-03 19:58:21 -0700

    timer

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
+var _ = require('lodash');
+var json = require('./data.json');
 const io = require('socket.io')();
 
 io.on('connection', (client) => {
   console.log('user connected. waiting for ready event...');
+  const newCardsSorted = _.orderBy(json, ['sent_at_second', 'id'], ['asc', 'desc']);  
 
   client.on('ready', () => {
     console.log('received ready event');
     console.log('starting timer and will check for new orders every second...');
     const interval = 1000; // 1 second
     let currentTime = 0; // start timer with 0 as initial value
-    const newCard = {
-      id: "01",
-      name: "Cheese Pizza",
-      event_name: "CREATED",
-      sent_at_second: 1,
-      destination: "1041 S Fairfax Ave, LA, CA 90019"
-    };
-    setInterval(() => {
-      console.log('emiting newCard event at time ' + currentTime + 's: ' + JSON.stringify(newCard));
-      client.emit('newCard', newCard);
+    let arrayIndex = 0;
+    let clock = setInterval(() => {
+      client.emit('timer', currentTime);
+      console.log('emiting timer event at time ' + currentTime + 's');
+      if (currentTime === newCardsSorted[arrayIndex].sent_at_second) {
+        const newCard = newCardsSorted[arrayIndex++];
+        console.log('emiting newCard event at time ' + currentTime + 's');
+        client.emit('newCard', newCard);
+
+        if(arrayIndex === newCardsSorted.length) {
+          clearInterval(clock);
+          console.log('Clock stopped at '+currentTime+'s');
+        }
+      }
       currentTime++;
     }, interval);
   });
