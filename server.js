5f6873685302af56496474cc54eabad942d8a010
Vardhan Dugar <vardhan1990@gmail.com>
2019-08-03 20:26:59 -0700

    everything works kinda

--- server.js
+++ server.js
@@ -4,27 +4,22 @@ const io = require('socket.io')();
 
 io.on('connection', (client) => {
   console.log('user connected. waiting for ready event...');
-  const newCardsSorted = _.orderBy(json, ['sent_at_second', 'id'], ['asc', 'desc']);  
 
   client.on('ready', () => {
     console.log('received ready event');
     console.log('starting timer and will check for new orders every second...');
     const interval = 1000; // 1 second
     let currentTime = 0; // start timer with 0 as initial value
-    let arrayIndex = 0;
     let clock = setInterval(() => {
-      client.emit('timer', currentTime);
       console.log('emiting timer event at time ' + currentTime + 's');
-      if (currentTime === newCardsSorted[arrayIndex].sent_at_second) {
-        const newCard = newCardsSorted[arrayIndex++];
-        console.log('emiting newCard event at time ' + currentTime + 's');
-        client.emit('newCard', newCard);
+      client.emit('timer', currentTime);
 
-        if(arrayIndex === newCardsSorted.length) {
-          clearInterval(clock);
-          console.log('Clock stopped at '+currentTime+'s');
-        }
+      const newCardsToBeSent = _.remove(json, newCard => newCard.sent_at_second <= currentTime);
+      if (newCardsToBeSent.length > 0) {
+        console.log('emiting newCards event at time ' + currentTime + 's');
+        client.emit('newCards', JSON.stringify(newCardsToBeSent));
       }
+
       currentTime++;
     }, interval);
   });
