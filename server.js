ce874194c0780fab23a80aa11dc22787dc46a004
Vardhan Dugar <vardhan1990@gmail.com>
2019-08-03 22:49:55 -0700

    rename

--- server.js
+++ server.js
@@ -6,21 +6,23 @@ io.on('connection', (client) => {
   console.log('user connected. waiting for ready event...');
 
   client.on('ready', () => {
-    console.log('received ready event');
-    console.log('starting timer and will check for new orders every second...');
-    const interval = 1000; // 1 second
-    let currentTime = 0; // start timer with 0 as initial value
-    let clock = setInterval(() => {
-      console.log('emiting timer event at time ' + currentTime + 's');
-      client.emit('timer', currentTime);
+    console.log('received ready event. starting clock...');
 
-      const newCardsToBeSent = _.remove(json, newCard => newCard.sent_at_second <= currentTime);
-      if (newCardsToBeSent.length > 0) {
-        console.log('emiting newCards event at time ' + currentTime + 's');
-        client.emit('newCards', JSON.stringify(newCardsToBeSent));
-      }
-
+    const interval = 1000; 
+    let currentTime = 0;
+    setInterval(() => {
       currentTime++;
+
+      // Emit clock tick event
+      console.log('emiting timer event at time ' + currentTime + 's');
+      client.emit('clockTick', currentTime);
+
+      // Emit new orders event
+      const newUpdatesToBeSent = _.filter(json, newUpdate => newUpdate.sent_at_second === currentTime);
+      if (newUpdatesToBeSent.length > 0) {
+        console.log('emiting newUpdates event at time ' + currentTime + 's');
+        client.emit('newUpdates', JSON.stringify(newUpdatesToBeSent));
+      }
     }, interval);
   });
 
